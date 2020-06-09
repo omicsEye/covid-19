adf41e716433f4d8c128789f610b0040fd1d8b08
grahnavard <gholamali.rahnavard@gmail.com>
2020-06-09 16:28:53 -0400

    minor changes

--- scripts/m2clust_run.sh
+++ scripts/m2clust_run.sh
@@ -1,8 +1,9 @@
 
-regions=("E" "M" "N" "ORF1a" "ORF1b" "ORF1ab" "ORF3a" "ORF6" "ORF7a" "ORF7b" "ORF8" "ORF10") 
+home_dir=~/Box/COVID19_Project
+regions=("S" "E" "M" "N" "ORF1a" "ORF1b" "ORF1ab" "ORF3a" "ORF6" "ORF7a" "ORF7b" "ORF8" "ORF10") 
 dist_method="TN93"
 for region in "${regions[@]}"
 do
-	m2clust -i ~/Box/COVID19_Project/data/Distances_0508/${dist_method}/${region}_500.tsv -o ~/Box/COVID19_Project/analysis/m2clust_0508/${dist_method}/${region}_500 --size-to-plot 100 --metadata ~/Box/COVID19_Project/data/MetaData/metadata.tsv
+	m2clust -i ${home_dir}/data/Distances_0508/${dist_method}/${region}_500.tsv -o ${home_dir}/analysis/m2clust_0508/${dist_method}/${region}_500 --metadata ${home_dir}/data/MetaData/metadata.tsv
 done
 echo "Done running m2clust"
--- scripts/metadata_cleaning.R
+++ scripts/metadata_cleaning.R
@@ -1,7 +1,5 @@
 setwd("~/Box/COVID19_Project")
 
-regions <- c("S", "E", "M", 'N', 'ORF1a', 'ORF1b', 'ORF1ab', 'ORF3a', 'ORF6', 'ORF7a', 'ORF7b', 'ORF8', 'ORF10') #'reference'
-
 metadata <- read.delim(
   'data/MetaData/genome_metadata_formatted.tsv',
   sep = '\t',
@@ -11,8 +9,16 @@ metadata <- read.delim(
   check.names = F,
   row.names = 1
 )
+
+# select the metadata of interest
 metadata2 <- metadata[, c("country", "country_exposure", "sex", "age")]
+
+# replace charecters to be properly NAs
 metadata2[metadata2 == '?'] <- NA
 metadata2[metadata2 == 'nan'] <- NA
+
+# categorize age as m2clust only accepts categorical data 
 metadata2$age <- cut(as.numeric(metadata2$age), breaks = seq(0,100,by=15), right = TRUE)
+
+# write the file
 write.table( metadata2,'data/MetaData/metadata.tsv' , sep = "\t", eol = "\n", col.names = NA, quote= F, row.names = T)
